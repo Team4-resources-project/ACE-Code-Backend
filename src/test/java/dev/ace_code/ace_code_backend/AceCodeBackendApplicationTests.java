3c1dfde6c3f8efffa0cc93c4160c5854a04bf2bf
AnaBHernandez <[anahernandezfit@gmail.com]>
2025-03-10 12:04:17 +0100

    Init project

---
+++ src/test/java/dev/ace_code/ace_code_backend/AceCodeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ace_code.ace_code_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceCodeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
