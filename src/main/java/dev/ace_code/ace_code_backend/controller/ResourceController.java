4bf444c5bfc554fc80b91505242b4ba1d9108807
Erica <you@example.com>
2025-03-11 18:38:23 +0100

    deleteResource at ResourceController created. Functional, test with postman done

--- src/main/java/dev/ace_code/ace_code_backend/controller/ResourceController.java
+++ src/main/java/dev/ace_code/ace_code_backend/controller/ResourceController.java
@@ -2,7 +2,10 @@ package dev.ace_code.ace_code_backend.controller;
 
 import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,5 +35,15 @@ public class ResourceController {
             } catch (IOException e) {
                 return ResponseEntity.status(500).body("Error al subir archivo");
             }
+    }
+
+    @DeleteMapping("/upload/{id}")
+    public ResponseEntity<String> deleteResource(@PathVariable Long id) {
+        boolean deleted = resourceService.deleteResource(id);
+        if (deleted) {
+            return ResponseEntity.ok("Recurso eliminado correctamente.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Recurso no encontrado.");
+        }
     }
 }
