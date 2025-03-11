7e1761a44828434533ce33ab4b88bda1b6bd9f04
Erica <you@example.com>
2025-03-11 16:38:38 +0100

    ResourceService created, directory uploads created with a file, data.sql changed to add file path and properties changed to add uploads path

---
+++ src/main/java/dev/ace_code/ace_code_backend/service/ResourceService.java
@@ -0,0 +1,46 @@
+package dev.ace_code.ace_code_backend.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import dev.ace_code.ace_code_backend.model.Resource;
+
+@Service
+public class ResourceService {
+    @Value("${resource.upload-dir}")
+    private String uploadDir;
+
+    private final ResourceRepository resourceRepository;
+
+    public ResourceService(ResourceRepository resourceRepository) {
+        this.resourceRepository = resourceRepository;
+    }
+
+    public Resource storeResource(MultipartFile file, String title, String category) throws IOException {
+        File directory = new File (uploadDir);
+        if(!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        String filePath = uploadDir + "/" + file.getOriginalFilename();
+        Path path = Paths.get(filePath);
+        Files.write(path, file.getBytes());
+        
+        String fileUrl = filePath;
+        Resource resource = new Resource(title, fileUrl, category);
+        return resourceRepository.save(resource);
+    }
+
+    public File getResource(Long id) {
+        return resourceRepository.findById(id)
+        .map(resource -> new File(resource.getFileUrl()))
+        .orElse(null);
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO resources (title, file_url, category) VALUES
                                                       ('ejercicio 1', '', 'exercises'),
                                                       ('video 1', '', 'tutorials'),
-                                                      ('titulo 1', '', 'documentation');
+                                                      ('titulo 1', './uploads/codigosostenible.pdf', 'documentation');
