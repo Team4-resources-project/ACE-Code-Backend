3696e6a96368315b57f72eec4df7723f976583a1
AnaBHernandez <[anahernandezfit@gmail.com]>
2025-03-13 08:10:35 +0100

    Eliminados los comentarios en la rama user

--- src/main/java/dev/ace_code/ace_code_backend/model/User.java
+++ src/main/java/dev/ace_code/ace_code_backend/model/User.java
@@ -14,20 +14,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username; // Cambiado de 'user' a 'username'
+    private String username; 
     private String password;
 
     public User() {}
 
-    public User(String username, String password) { // Cambiado 'user' a 'username'
-        this.username = username; // Cambiado 'user' a 'username'
+    public User(String username, String password) {
+        this.username = username; 
         this.password = password;
     }
 
     public Long getId() { return id; }
 
-    public String getUsername() { return username; } // Cambiado 'getUser' a 'getUsername'
-    public void setUsername(String username) { this.username = username; } // Cambiado 'setUser' a 'setUsername'
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
