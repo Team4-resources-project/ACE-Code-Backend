7c1f307b1a842521f7aaeb0098abecb2d2dbe6d4
erikamc99 <erikamc99@gmail.com>
2025-03-14 09:31:26 +0100

    CORS config changed to adapt to front, front and back connected

--- src/main/java/dev/ace_code/ace_code_backend/controller/CORSConfig.java
+++ src/main/java/dev/ace_code/ace_code_backend/controller/CORSConfig.java
@@ -15,7 +15,7 @@ public class CORSConfig {
                 @Override
                 public void addCorsMappings(@NonNull CorsRegistry registry) {
                     registry.addMapping("/**")
-                            .allowedOrigins("http://localhost:3000")
+                            .allowedOrigins("http://localhost:5173")
                             .allowedMethods("GET", "POST", "PUT", "DELETE");
                 }
             };
