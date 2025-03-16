bb35d9f7dae62fc872693f540a63a14f140bf7ba
Erica <you@example.com>
2025-03-16 14:45:08 +0100

    change fileUrl to can visualize files and change exercises to png

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,23 @@
 INSERT INTO resources (title, file_url, category) VALUES
-              ('Ejercicio 1', './uploads/Ejercicio1.pdf', 'exercises'),
-              ('Ejercicio 2', './uploads/Ejercicio2.pdf', 'exercises'),
-              ('Ejercicio 3', './uploads/Ejercicio3.pdf', 'exercises'),
-              ('Ejercicio 4', './uploads/Ejercicio4.pdf', 'exercises'),
-              ('Ejercicio 5', './uploads/Ejercicio5.pdf', 'exercises'),
-              ('Ejercicio 6', './uploads/Ejercicio6.pdf', 'exercises'),
+              ('Ejercicio 1', 'http://localhost:8080/resources/upload/files/Ejercicio1.PNG' , 'exercises'),
+              ('Ejercicio 2', 'http://localhost:8080/resources/upload/files/Ejercicio2.PNG', 'exercises'),
+              ('Ejercicio 3', 'http://localhost:8080/resources/upload/files/Ejercicio3.PNG', 'exercises'),
+              ('Ejercicio 4', 'http://localhost:8080/resources/upload/files/Ejercicio4.PNG', 'exercises'),
+              ('Ejercicio 5', 'http://localhost:8080/resources/upload/files/Ejercicio5.PNG', 'exercises'),
+              ('Ejercicio 6', 'http://localhost:8080/resources/upload/files/Ejercicio6.PNG', 'exercises'),
               ('Deploy a React Vite App', 'https://www.youtube.com/watch?v=hn1IkJk24ow', 'tutorials'),
               ('React Color Picker', 'https://www.youtube.com/watch?v=3hCN3dLHV24&t=3s', 'tutorials'),
               ('JavaScript tutorial for beginners', 'https://www.youtube.com/watch?v=Ihy0QziLDf0&list=PLZPZq0r_RZOO1zkgO4bIdfuLpizCeHYKv', 'tutorials'),
               ('Aprendiendo Javascript y React desde cero', 'https://www.youtube.com/watch?v=YLvT1ELnaX4', 'tutorials'),
               ('Desarrolla con JavaScript sin bibliotecas un mini proyecto hasta producción (para principiantes)', 'https://www.youtube.com/watch?v=6AMKwVcpYTk&t=44s', 'tutorials'),
               ('SIMULACRO de PRUEBA TÉCNICA de PROGRAMADOR para TRABAJO en REMOTO en EEUU', 'https://www.youtube.com/watch?v=xkXtqa8kjFY', 'tutorials'),
-              ('Introducción', './uploads/intro.pdf', 'documentation'),
-              ('Código sostenible', './uploads/codigosostenible.pdf', 'documentation'),
-              ('Funciones', './uploads/FuncionesJS.pdf', 'documentation'),
-              ('DOM', './uploads/DOM.pdf', 'documentation'),
-              ('Asincronía', './uploads/JSasincrono.pdf', 'documentation'),
-              ('Callbacks', './uploads/callbacks.pdf', 'documentation'),
-              ('Web APIs', './uploads/api.pdf', 'documentation');
+              ('Introducción', 'http://localhost:8080/resources/upload/files/intro.pdf', 'documentation'),
+              ('Código sostenible', 'http://localhost:8080/resources/upload/files/codigosostenible.pdf', 'documentation'),
+              ('Funciones', 'http://localhost:8080/resources/upload/files/FuncionesJS.pdf', 'documentation'),
+              ('DOM', 'http://localhost:8080/resources/upload/files/DOM.pdf', 'documentation'),
+              ('Asincronía', 'http://localhost:8080/resources/upload/files/JSasincrono.pdf', 'documentation'),
+              ('Callbacks', 'http://localhost:8080/resources/upload/files/callbacks.pdf', 'documentation'),
+              ('Web APIs', 'http://localhost:8080/resources/upload/files/api.pdf', 'documentation');
 
 
 INSERT INTO users (username, password) VALUES
