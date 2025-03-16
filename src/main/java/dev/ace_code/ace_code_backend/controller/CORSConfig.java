8d0453361475dfe8851d9fee01ed774a9718d5be
Erica <you@example.com>
2025-03-16 22:56:34 +0100

    Change CORS Config to add front-end deployment page

--- src/main/java/dev/ace_code/ace_code_backend/controller/CORSConfig.java
+++ src/main/java/dev/ace_code/ace_code_backend/controller/CORSConfig.java
@@ -15,7 +15,7 @@ public class CORSConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","http://localhost:5174", "https://team4-resources-project.github.io/ACE-Code-Frontend/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
