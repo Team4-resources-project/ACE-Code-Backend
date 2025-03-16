e9d426f38e657d71e7bfe9516792b58b611c9b96
Erica <you@example.com>
2025-03-16 13:43:39 +0100

    exercises and tutorials added, data.sql updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,16 @@
 INSERT INTO resources (title, file_url, category) VALUES
-
-              ('ejercicio 1', '', 'exercises'),
-              ('video 1', '', 'tutorials'),
+              ('Ejercicio 1', './uploads/Ejercicio1.pdf', 'exercises'),
+              ('Ejercicio 2', './uploads/Ejercicio2.pdf', 'exercises'),
+              ('Ejercicio 3', './uploads/Ejercicio3.pdf', 'exercises'),
+              ('Ejercicio 4', './uploads/Ejercicio4.pdf', 'exercises'),
+              ('Ejercicio 5', './uploads/Ejercicio5.pdf', 'exercises'),
+              ('Ejercicio 6', './uploads/Ejercicio6.pdf', 'exercises'),
+              ('Deploy a React Vite App', 'https://www.youtube.com/watch?v=hn1IkJk24ow', 'tutorials'),
+              ('React Color Picker', 'https://www.youtube.com/watch?v=3hCN3dLHV24&t=3s', 'tutorials'),
+              ('JavaScript tutorial for beginners', 'https://www.youtube.com/watch?v=Ihy0QziLDf0&list=PLZPZq0r_RZOO1zkgO4bIdfuLpizCeHYKv', 'tutorials'),
+              ('Aprendiendo Javascript y React desde cero', 'https://www.youtube.com/watch?v=YLvT1ELnaX4', 'tutorials'),
+              ('Desarrolla con JavaScript sin bibliotecas un mini proyecto hasta producción (para principiantes)', 'https://www.youtube.com/watch?v=6AMKwVcpYTk&t=44s', 'tutorials'),
+              ('SIMULACRO de PRUEBA TÉCNICA de PROGRAMADOR para TRABAJO en REMOTO en EEUU', 'https://www.youtube.com/watch?v=xkXtqa8kjFY', 'tutorials'),
               ('Introducción', './uploads/intro.pdf', 'documentation'),
               ('Código sostenible', './uploads/codigosostenible.pdf', 'documentation'),
               ('Funciones', './uploads/FuncionesJS.pdf', 'documentation'),
@@ -15,4 +24,3 @@ INSERT INTO users (username, password) VALUES
 ('user1', 'password123'),
 ('user2', 'mypassword'),
 ('user3', 'secretpassword');
-
